796ae7a514c73c231af985545bd5e46367c9f63a
Subhashini <subhashinikumar2004@gmail.com>
2025-01-25 15:47:16 +0530

    Added all modules after cors installation

---
+++ middlewares/auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleWare = (req,res,next) =>{
+
+    // req.headers= {
+    //     Authorization : "Bearer 1234567890",
+    // };
+
+    const token = req.header("Authorization")?.split(" ")[1]; //? - null checker
+    if(!token){
+        return res.status(401).send({message: "Access denied. No token provided"});
+    }
+    try{
+const decoded = jwt.verify(token,"my_secret");
+req.user = decoded   
+next()
+    }
+    catch(error){
+return res.status(400).send({message:"Invalid token"});
+    }
+};
+
+module.exports = authMiddleWare
